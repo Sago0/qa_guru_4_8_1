3c5b7b96d4ba075af6dfd010a773157d724cf2bd
Sago0 <sergey.gapeev@okay.qa>
2022-08-09 11:05:47 +0300

    added test, test base, allure, listener

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,16 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+
+public class TestBase {
+
+    @BeforeAll
+    static void setup(){
+        Configuration.browserSize = "2560x1440";
+        addListener("AllureSelenide", new AllureSelenide());
+    }
+}
---
+++ src/test/java/tests/YandexTests.java
@@ -0,0 +1,23 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class YandexTests extends TestBase {
+
+    @Test
+    void selenideSearchTest() {
+        // Открыть google
+        open("https://yandex.by/");
+
+        // Ввести Selenide в поиск
+        $("#text").setValue("Selenide").pressEnter();
+
+        // Проверить, что Selenide появился в результатах поиска
+         $(".main__content").shouldHave(text("selenide.org"));
+    }
+}
